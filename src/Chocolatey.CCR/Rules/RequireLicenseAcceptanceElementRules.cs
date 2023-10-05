dd960e765bde6672a4699af25ef8fabfccd7875d
AdmiringWorm <kim@chocolatey.io>
2023-10-05 12:28:20 +0200

    (#7) Add rule about license acceptance requiring URL
    This rule handles situations where a user has enabled the requirement
    of accepting the license, without them having specified a license URL
    yet.

---
+++ src/Chocolatey.CCR/Rules/RequireLicenseAcceptanceElementRules.cs
@@ -0,0 +1,29 @@
+namespace Chocolatey.CCR.Rules
+{
+    using System;
+    using System.Collections.Generic;
+    using chocolatey.infrastructure.rules;
+
+    public sealed class RequireLicenseAcceptanceElementRules : CCRMetadataRuleBase
+    {
+        private const string RuleId = "CPMR0007";
+
+        public override IEnumerable<RuleResult> Validate(global::NuGet.Packaging.NuspecReader reader)
+        {
+            if (reader is null)
+            {
+                throw new ArgumentNullException(nameof(reader));
+            }
+
+            if (reader.GetRequireLicenseAcceptance() && string.IsNullOrEmpty(reader.GetLicenseUrl()))
+            {
+                yield return GetRule(RuleId);
+            }
+        }
+
+        protected internal override IEnumerable<(RuleType severity, string? id, string summary)> GetRulesInformation()
+        {
+            yield return (RuleType.Error, RuleId, "The license URL is absent, despite the requirement for license acceptance.");
+        }
+    }
+}
