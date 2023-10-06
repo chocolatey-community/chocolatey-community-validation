e67cb0792de5d1efb29369f0b8e8a6d7787cebff
AdmiringWorm <kim@chocolatey.io>
2023-10-06 17:31:22 +0200

    (#15) Add description rule to not exceed 4000 characters
    This commit adds a new rule to validate that the description do not
    contain more than 4000 characters.
    
    There is a limit when pushing a package to Chocolatey Community
    Repository that will reject any requests with characters above 4000
    characters. As such we should validate this when creating the package.

--- src/Chocolatey.CCR/Rules/DescriptionElementRules.cs
+++ src/Chocolatey.CCR/Rules/DescriptionElementRules.cs
@@ -2,11 +2,13 @@ namespace Chocolatey.CCR.Rules
 {
     using System;
     using System.Collections.Generic;
+    using System.Text;
     using chocolatey.infrastructure.rules;
 
     public sealed class DescriptionElementRules : CCRMetadataRuleBase
     {
         private const string RuleId = "CPMR0002";
+        private const string TooManyCharsRuleId = "CPMR0026";
 
         public override IEnumerable<RuleResult> Validate(global::NuGet.Packaging.NuspecReader reader)
         {
@@ -21,11 +23,16 @@ namespace Chocolatey.CCR.Rules
             {
                 yield return GetRule(RuleId);
             }
+            else if (description.Length > 4000)
+            {
+                yield return GetRule(TooManyCharsRuleId, $"The description had a length of {description.Length:N0} characters. A description can not have a length of above {4000:N0} characters.");
+            }
         }
 
         protected internal override IEnumerable<(RuleType severity, string? id, string summary)> GetRulesInformation()
         {
             yield return (RuleType.Error, RuleId, "A description of the package is either missing or empty.");
+            yield return (RuleType.Error, TooManyCharsRuleId, $"A description can not have a length of above {4000:N0} characters.");
         }
     }
 }
