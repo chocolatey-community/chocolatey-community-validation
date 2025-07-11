f9fb0b91619c596544ba4cdb94aa7b1c7c1caea0
Gary Ewan Park <gep13@gep13.co.uk>
2025-07-11 11:41:42 +0100

    (build) Add more build configurations
    This commit overhauls the existing build configuration to match what is
    
     - currently done in the Chocolatey CLI build, this includes:
     - that only unit tests are executed
     - that the Slack Notification template is being used
     - only certain branches trigger builds
    
    In addition, a two new build configurations are introduced.  One to run
    scheuled integration tests, and another to run scheduled SonarQube
    analysis.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,17 +2,23 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.nuGetPublish
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.ScheduleTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 project {
     buildType(ChocolateyCommunityValidation)
+    buildType(ChocolateyCommunityValidationSchd)
+    buildType(ChocolateyCommunityValidationQA)
 }
 
 object ChocolateyCommunityValidation : BuildType({
     id = AbsoluteId("ChocolateyCommunityValidation")
-    name = "Build"
+    name = "Build (Unit Tests)"
+
+    templates(AbsoluteId("SlackNotificationTemplate"))
 
     artifactRules = """
     """.trimIndent()
@@ -42,26 +48,18 @@ object ChocolateyCommunityValidation : BuildType({
         script {
             name = "Call Cake"
             scriptContent = """
-                IF "%teamcity.build.triggeredBy%" == "Schedule Trigger" (SET TestType=all) ELSE (SET TestType=unit)
-                call build.official.bat --verbosity=diagnostic --target=CI --testExecutionType=%%TestType%% --shouldRunOpenCover=false
+                build.official.bat --verbosity=diagnostic --target=CI --testExecutionType=unit --shouldRunOpenCover=false
             """.trimIndent()
         }
     }
 
     triggers {
         vcs {
-            branchFilter = ""
-        }
-        schedule {
-            schedulingPolicy = daily {
-                hour = 2
-                minute = 50
-            }
             branchFilter = """
-                +:<default>
+                +:*
+                -:master
+                -:support/*
             """.trimIndent()
-            triggerBuild = always()
-			withPendingChangesOnly = false
         }
     }
 
@@ -72,6 +70,106 @@ object ChocolateyCommunityValidation : BuildType({
                     token = "%system.GitHubPAT%"
                 }
             }
+        }
+    }
+})
+
+object ChocolateyCommunityValidationSchd : BuildType({
+    id = AbsoluteId("ChocolateyCommunityValidationSchd")
+    name = "Build (Scheduled Integration Testing)"
+
+    templates(AbsoluteId("SlackNotificationTemplate"))
+
+    artifactRules = """
+    """.trimIndent()
+
+    params {
+        password("env.EAZFUSCATOR_NET_LICENSE", "credentialsJSON:cdac4820-7cd7-54e4-b539-8ecaa3490a3c", display = ParameterDisplay.HIDDEN, readOnly = true)
+        param("env.vcsroot.branch", "%vcsroot.branch%")
+        param("env.Git_Branch", "%teamcity.build.vcs.branch.ChocolateyCommunityValidation_CommunityValVcsRoot%")
+        param("teamcity.git.fetchAllHeads", "true")
+        password("env.GITHUB_PAT", "%system.GitHubPAT%", display = ParameterDisplay.HIDDEN, readOnly = true)
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        branchFilter = """
+            +:*
+        """.trimIndent()
+    }
+
+    steps {
+        script {
+            name = "Call Cake"
+            scriptContent = """
+                build.official.bat --verbosity=diagnostic --target=CI --testExecutionType=all --shouldRunOpenCover=false --shouldRunAnalyze=false --shouldRunIlMerge=false --shouldObfuscateOutputAssemblies=false --shouldRunChocolatey=false --shouldRunNuGet=false --shouldAuthenticodeSignOutputAssemblies=false --shouldAuthenticodeSignPowerShellScripts=false
+            """.trimIndent()
+        }
+    }
+
+    triggers {
+        schedule {
+            schedulingPolicy = daily {
+                hour = 2
+                minute = 0
+            }
+            branchFilter = """
+                +:<default>
+            """.trimIndent()
+            triggerBuild = always()
+            withPendingChangesOnly = false
+        }
+    }
+})
+
+object ChocolateyCommunityValidationQA : BuildType({
+    id = AbsoluteId("ChocolateyCommunityValidationQA")
+    name = "Build (SonarQube)"
+
+    templates(AbsoluteId("SlackNotificationTemplate"))
+
+    artifactRules = """
+    """.trimIndent()
+
+    params {
+        password("env.EAZFUSCATOR_NET_LICENSE", "credentialsJSON:cdac4820-7cd7-54e4-b539-8ecaa3490a3c", display = ParameterDisplay.HIDDEN, readOnly = true)
+        param("env.vcsroot.branch", "%vcsroot.branch%")
+        param("env.Git_Branch", "%teamcity.build.vcs.branch.ChocolateyCommunityValidation_CommunityValVcsRoot%")
+        param("env.SONARQUBE_ID", "chocolatey-community-validation")
+        param("teamcity.git.fetchAllHeads", "true")
+        password("env.GITHUB_PAT", "%system.GitHubPAT%", display = ParameterDisplay.HIDDEN, readOnly = true)
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        branchFilter = """
+            +:*
+        """.trimIndent()
+    }
+
+    steps {
+        script {
+            name = "Call Cake"
+            scriptContent = """
+                build.official.bat --verbosity=diagnostic --target=CI --testExecutionType=none --shouldRunSonarQube=true --shouldRunDependencyCheck=true --shouldRunOpenCover=false --shouldRunAnalyze=false --shouldRunIlMerge=false --shouldObfuscateOutputAssemblies=false --shouldRunChocolatey=false --shouldRunNuGet=false --shouldAuthenticodeSignOutputAssemblies=false --shouldAuthenticodeSignPowerShellScripts=false
+            """.trimIndent()
+        }
+    }
+
+    triggers {
+        schedule {
+            schedulingPolicy = weekly {
+                dayOfWeek = ScheduleTrigger.DAY.Saturday
+                hour = 2
+                minute = 45
+            }
+            branchFilter = """
+                +:<default>
+            """.trimIndent()
+            triggerBuild = always()
+            withPendingChangesOnly = false
         }
     }
 })
